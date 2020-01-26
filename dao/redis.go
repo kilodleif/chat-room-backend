10303b44ac7299348446cba555a96dd3d11f59ce
kilodleif <kilodleif@gmail.com>
2020-01-27 00:14:39 +0800

    bug fix

--- dao/redis.go
+++ dao/redis.go
@@ -5,15 +5,16 @@ import (
 	"github.com/go-redis/redis"
 	"gochatroom/chat"
 	"log"
+	"time"
 )
 
-// gochat.room.<room_id>      --> json string
-// gochat.member.<member_id>  --> json string
+// string:  gochat.room.<room_id>      --> json string
+// hash:    gochat.member  key  --> json string
 const (
-	// 聊天室redis key前缀
+	// 聊天室string key前缀
 	roomKeyPrefix = "gochat:room:"
-	// 用户redis key前缀
-	memberKeyPrefix = "gochat:member:"
+	// 用户hash key
+	memberKey = "gochat:member"
 )
 
 var client *redis.Client
@@ -33,26 +34,18 @@ func SaveRoom(roomId string, room *chat.Room)  {
 		log.Println("序列化room时出错", err)
 	}
 	key := roomKeyPrefix + roomId
-	client.RPush(key, string(marshal))
+	client.Set(key, string(marshal), 36 * time.Hour)
 }
 
-// 获取所有聊天室信息
-func RetrieveRooms(roomId string) []chat.Room {
-	var rooms []chat.Room
-	key := roomKeyPrefix + roomId
-	result, err := client.LRange(key, 0, -1).Result()
+// 获取指定聊天室信息
+func RetrieveRoom(roomId string) chat.Room {
+	result := client.Get(roomId).String()
+	room := &chat.Room{}
+	err := json.Unmarshal([]byte(result), room)
 	if err != nil {
-		log.Println("获取room结果时出错", err)
+		log.Println("反序列化room结果时出错", err)
 	}
-	for _, str := range result {
-		room := &chat.Room{}
-		err := json.Unmarshal([]byte(str), room)
-		if err != nil {
-			log.Println("反序列化room结果时出错", err)
-		}
-		rooms = append(rooms, *room)
-	}
-	return rooms
+	return *room
 }
 
 // 保存用户信息
@@ -61,25 +54,16 @@ func SaveMember(memberId string, member *chat.Member)  {
 	if err != nil {
 		log.Println("序列化member时出错", err)
 	}
-	key := memberKeyPrefix + memberId
-	client.RPush(key, string(marshal))
+	client.HSet(memberKey, memberId, string(marshal))
 }
 
-// 获取所有用户信息
-func RetrieveMembers(memberId string) []chat.Member {
-	var members []chat.Member
-	key := memberKeyPrefix + memberId
-	result, err := client.LRange(key, 0, -1).Result()
+// 获取指定用户信息
+func RetrieveMember(memberId string) chat.Member {
+	result := client.HGet(memberKey, memberId).String()
+	member := &chat.Member{}
+	err := json.Unmarshal([]byte(result), member)
 	if err != nil {
-		log.Println("获取member结果时出错", err)
+		log.Println("反序列化member结果时出错", err)
 	}
-	for _, str := range result {
-		member := &chat.Member{}
-		err := json.Unmarshal([]byte(str), member)
-		if err != nil {
-			log.Println("反序列化member结果时出错", err)
-		}
-		members = append(members, *member)
-	}
-	return members
+	return *member
 }
